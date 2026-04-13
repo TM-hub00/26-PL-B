3ff7b1ec0f6bc37e4ae0ec776dece6922d03ba38
Taemin Roh <univtm@hansung.ac.kr>
2026-04-13 12:23:49 +0900

    260413

---
+++ a.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello world\n");
+	printf("HELLO WORLD\n");
+	printf("my name is RTM\n");
+}
---
+++ say-age/getage.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int get_age()
+{
+	int age;
+
+	printf("input your age: ");
+	scanf("%d", &age);
+
+	return age;
+}
+
